614d7ce25bc8c3505d0fdfc2f4542dfcd65a9a69
Vitória Bezerra <56508704+vitoriabezerra@users.noreply.github.com>
2023-04-04 10:47:30 -0300

    testing button and modal with ngx bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, TemplateRef } from '@angular/core'
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal'
 
 @Component({
     selector: 'app-root',
@@ -8,4 +9,25 @@ import { Component } from '@angular/core'
 export class AppComponent {
     title = 'test-project'
     userName = 'Vitória'
+    modalRef?: BsModalRef
+    message?: string
+    constructor(private modalService: BsModalService) {}
+
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template)
+    }
+
+    openSecondModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template, { class: 'modal-sm' })
+    }
+
+    confirm(): void {
+        this.message = 'Sim'
+        this.modalRef?.hide()
+    }
+
+    decline(): void {
+        this.message = 'Não'
+        this.modalRef?.hide()
+    }
 }
